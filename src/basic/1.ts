ff2de32d3f9181cfc989cace435e72ba56d72465
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-07-09 21:50:31 +0300

    basic done

---
+++ src/basic/1.ts
@@ -0,0 +1,12 @@
+let age: number;
+let userName: string;
+let toggle: boolean;
+let empty: null;
+let callback = (a: number): number => {
+  return 100 + a;
+};
+age = 50;
+userName = "Max";
+toggle = true;
+empty = null;
+callback(100);
---
+++ src/basic/3.ts
@@ -0,0 +1,6 @@
+let uniType: string | number;
+uniType = "Hello";
+uniType = 10;
+
+let oneOfValue: "enable" | "disable";
+oneOfValue = "enable";
