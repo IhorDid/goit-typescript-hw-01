a6de7966898e99fe8890cea278911f32b35d09d9
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-07-10 21:31:29 +0300

    type generic-3

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -8,8 +8,8 @@
 // }
 
 // **************************
-function merge<T extends object, U extends object>(objA: T, objB: U): object {
-  return Object.assign(objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign({}, objA, objB);
 }
 
 console.log(merge);
